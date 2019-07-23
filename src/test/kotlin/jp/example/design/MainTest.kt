cba68c49f269deeb26e03895fa1993558533e226
jintz <keisuke.igimi@gmail.com>
2019-07-23 17:28:50 +0900

    テスト失敗の確認

--- src/test/kotlin/jp/example/design/MainTest.kt
+++ src/test/kotlin/jp/example/design/MainTest.kt
@@ -1,5 +1,7 @@
 package jp.example.design
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.platform.suite.api.SelectPackages
 import org.junit.platform.suite.api.SuiteDisplayName
@@ -8,4 +10,9 @@ import org.junit.runner.RunWith
 @RunWith(JUnitPlatform::class)
 @SuiteDisplayName("All test suite.")
 @SelectPackages("jp.example.design")
-class MainTest
+class MainTest {
+    @Test
+    fun failingTest() {
+        fail { "Confirmation of Travis.CI test failing." }
+    }
+}
